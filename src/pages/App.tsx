a960d4f4d6315e18b32d51b2fccdf00dfbd5a022
yuanzhicheng <313640087@qq.com>
2020-10-11 01:26:41 +0800

    pizza interface

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -5,7 +5,7 @@ import styled from 'styled-components'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Popups from '../components/Popups'
- // import TrcWeb3ReactManager from '../components/TrcWeb3ReactManager'
+// import TrcWeb3ReactManager from '../components/TrcWeb3ReactManager'
 // import DarkModeQueryParamReader from '../theme/DarkModeQueryParamReader'
 import AddLiquidity from './AddLiquidity'
 import {
@@ -27,6 +27,7 @@ import Home from './Home'
 import Rules from './rules'
 import { RedirectPathToSwapOnly, RedirectToSwap } from './Swap/redirects'
 import './app.css'
+
 const AppWrapper = styled.div`
     background-color: #f0e7ea;
     font-family: Nunito,sans-serif;
@@ -53,6 +54,14 @@ const BodyWrapper = styled.div`
   overflow-x: hidden;
   z-index: 10;
 `
+const SecWrapper = styled.div`
+  height: 100px;
+width: 100%;
+position: fixed;
+bottom: 0;
+right:0;
+text-align:right
+`
 
 const Marginer = styled.div`
   margin-top: 5rem;
@@ -66,39 +75,43 @@ export default function App() {
         {/*<Route component={DarkModeQueryParamReader} />*/}
         <AppWrapper>
           <HeaderWrapper>
-            <Header />
+            <Header/>
           </HeaderWrapper>
           <BodyWrapper>
-            <Popups />
+            <Popups/>
             {/*<TrcWeb3ReactManager>*/}
-              <Switch>
-                <Route exact strict path="/home" component={Home} />
-                <Route exact strict path="/swap" component={Swap} />
-                <Route exact strict path="/Menu" component={Farms} />
-                <Route exact strict path="/Menu/:symbol" component={Detail}/>
-                {/*Rules*/}
-                <Route exact strict path="/Rules" component={Rules} />
-                <Route exact strict path="/swap/:outputCurrency" component={RedirectToSwap} />
-                <Route exact strict path="/send" component={RedirectPathToSwapOnly} />
-                <Route exact strict path="/find" component={PoolFinder} />
-                <Route exact strict path="/pool" component={Pool} />
-                <Route exact strict path="/create" component={RedirectToAddLiquidity} />
-                <Route exact path="/add" component={AddLiquidity} />
-                <Route exact path="/add/:currencyIdA" component={RedirectOldAddLiquidityPathStructure} />
-                <Route exact path="/add/:currencyIdA/:currencyIdB" component={RedirectDuplicateTokenIds} />
-                <Route exact strict path="/remove/v1/:address" component={RemoveV1Exchange} />
-                <Route exact strict path="/remove/:tokens" component={RedirectOldRemoveLiquidityPathStructure} />
-                <Route exact strict path="/remove/:currencyIdA/:currencyIdB" component={RemoveLiquidity} />
-                <Route exact strict path="/migrate/v1" component={MigrateV1} />
-                <Route exact strict path="/migrate/v1/:address" component={MigrateV1Exchange} />
-                <Route component={RedirectPathToSwapOnly} />
-              </Switch>
+            <Switch>
+              <Route exact strict path="/home" component={Home}/>
+              <Route exact strict path="/swap" component={Swap}/>
+              <Route exact strict path="/Menu" component={Farms}/>
+              <Route exact strict path="/Menu/:symbol" component={Detail}/>
+              {/*Rules*/}
+              <Route exact strict path="/Rules" component={Rules}/>
+              <Route exact strict path="/swap/:outputCurrency" component={RedirectToSwap}/>
+              <Route exact strict path="/send" component={RedirectPathToSwapOnly}/>
+              <Route exact strict path="/find" component={PoolFinder}/>
+              <Route exact strict path="/pool" component={Pool}/>
+              <Route exact strict path="/create" component={RedirectToAddLiquidity}/>
+              <Route exact path="/add" component={AddLiquidity}/>
+              <Route exact path="/add/:currencyIdA" component={RedirectOldAddLiquidityPathStructure}/>
+              <Route exact path="/add/:currencyIdA/:currencyIdB" component={RedirectDuplicateTokenIds}/>
+              <Route exact strict path="/remove/v1/:address" component={RemoveV1Exchange}/>
+              <Route exact strict path="/remove/:tokens" component={RedirectOldRemoveLiquidityPathStructure}/>
+              <Route exact strict path="/remove/:currencyIdA/:currencyIdB" component={RemoveLiquidity}/>
+              <Route exact strict path="/migrate/v1" component={MigrateV1}/>
+              <Route exact strict path="/migrate/v1/:address" component={MigrateV1Exchange}/>
+              <Route component={RedirectPathToSwapOnly}/>
+            </Switch>
             {/*</TrcWeb3ReactManager>*/}
-            <Marginer />
+            <Marginer/>
           </BodyWrapper>
+
           <Footer/>
         </AppWrapper>
       </HashRouter>
+      <SecWrapper>
+        <img src={require("../assets/images/sec.png")}/>
+      </SecWrapper>
     </Suspense>
   )
 }
