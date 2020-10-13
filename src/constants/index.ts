69df80dbe1939a848de2930d0c350a94d50880a9
yuanzhicheng <313640087@qq.com>
2020-10-14 01:51:22 +0800

    pizza interface

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -16,7 +16,7 @@ export const defRefAddress = '0x0513Dc7403e074F5C77368eE2819fA3A65b5cf80'
 // 空头合约
 export const AIR_POOL_ADDRESS = '0xee26e5F2F6F8695369Fec045dA1c4B4514C65cD7'
 
-export const POOL_ADDRESS = '0xf6d37894BFfFC0d5380C67E4aB7e5D13541A64aB'
+export const POOL_ADDRESS = '0xd70A0F5aC668D81CC461BBB7074FA705979B34B9'
 export const mainToken = {
   name:"Lef ",
   symbol: 'LEF',
