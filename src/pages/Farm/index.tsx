fefb20c0bd9246f67e5d59f0ef5ec3eabe5c5ba6
yuanzhicheng <313640087@qq.com>
2020-10-23 11:20:19 +0800

    pizza interface

--- src/pages/Farm/index.tsx
+++ src/pages/Farm/index.tsx
@@ -476,17 +476,17 @@ export default function Farm(props: RouteComponentProps<{ symbol: string }>) {
 
                   </RowItemButton>
 
-                  <RowItemButton color="#d16c00" font-size="16">
-                    {
-                      !account?<span className="sc-AxirZ kRQAGp" style={{color:'#999'}} >
-                         {t("Approval")}
-                      </span>: allow ? <a className="sc-AxirZ kRQAGp" href={'javascript:void(0)'} onClick={()=>{setVisibleModal(true);setPid(0)}}>
-                        {t("stake")}
-                      </a> : <a className="sc-AxirZ kRQAGp" href={'javascript:void(0)'} onClick={()=>account?approvalHandel():console.log("111")}>
-                        {t("Approval")}
-                      </a>
-                    }
-                  </RowItemButton>
+                  {/*<RowItemButton color="#d16c00" font-size="16">*/}
+                    {/*{*/}
+                      {/*!account?<span className="sc-AxirZ kRQAGp" style={{color:'#999'}} >*/}
+                         {/*{t("Approval")}*/}
+                      {/*</span>: allow ? <a className="sc-AxirZ kRQAGp" href={'javascript:void(0)'} onClick={()=>{setVisibleModal(true);setPid(0)}}>*/}
+                        {/*{t("stake")}*/}
+                      {/*</a> : <a className="sc-AxirZ kRQAGp" href={'javascript:void(0)'} onClick={()=>account?approvalHandel():console.log("111")}>*/}
+                        {/*{t("Approval")}*/}
+                      {/*</a>*/}
+                    {/*}*/}
+                  {/*</RowItemButton>*/}
                 </FlexCenter>
               </RowItemBox>
             </div>
@@ -529,18 +529,18 @@ export default function Farm(props: RouteComponentProps<{ symbol: string }>) {
                     }
 
                   </RowItemButton>
-                  <RowItemButton color="#d16c00" font-size="16">
+                  {/*<RowItemButton color="#d16c00" font-size="16">*/}
 
-                    {
-                      !account?<span className="sc-AxirZ kRQAGp" style={{color:'#999'}} >
-                         {t("Approval")}
-                      </span>: allow ? <a className="sc-AxirZ kRQAGp" href={'javascript:void(0)'} onClick={()=>{setVisibleModal(true);setPid(1)}}>
-                        {t("stake")}
-                      </a> : <a className="sc-AxirZ kRQAGp" href={'javascript:void(0)'} onClick={()=>account?approvalHandel():console.log("111")}>
-                        {t("Approval")}
-                      </a>
-                    }
-                  </RowItemButton>
+                    {/*{*/}
+                      {/*!account?<span className="sc-AxirZ kRQAGp" style={{color:'#999'}} >*/}
+                         {/*{t("Approval")}*/}
+                      {/*</span>: allow ? <a className="sc-AxirZ kRQAGp" href={'javascript:void(0)'} onClick={()=>{setVisibleModal(true);setPid(1)}}>*/}
+                        {/*{t("stake")}*/}
+                      {/*</a> : <a className="sc-AxirZ kRQAGp" href={'javascript:void(0)'} onClick={()=>account?approvalHandel():console.log("111")}>*/}
+                        {/*{t("Approval")}*/}
+                      {/*</a>*/}
+                    {/*}*/}
+                  {/*</RowItemButton>*/}
 
                 </FlexCenter>
               </RowItemBox>
@@ -584,18 +584,18 @@ export default function Farm(props: RouteComponentProps<{ symbol: string }>) {
                     }
 
                   </RowItemButton>
-                  <RowItemButton color="#d16c00" font-size="16">
+                  {/*<RowItemButton color="#d16c00" font-size="16">*/}
 
-                    {
-                      !account?<span className="sc-AxirZ kRQAGp" style={{color:'#999'}} >
-                         {t("Approval")}
-                      </span>: allow ? <a className="sc-AxirZ kRQAGp" href={'javascript:void(0)'} onClick={()=>{setVisibleModal(true);setPid(2)}}>
-                        {t("stake")}
-                      </a> : <a className="sc-AxirZ kRQAGp" href={'javascript:void(0)'} onClick={()=>account?approvalHandel():console.log("111")}>
-                        {t("Approval")}
-                      </a>
-                    }
-                  </RowItemButton>
+                    {/*{*/}
+                      {/*!account?<span className="sc-AxirZ kRQAGp" style={{color:'#999'}} >*/}
+                         {/*{t("Approval")}*/}
+                      {/*</span>: allow ? <a className="sc-AxirZ kRQAGp" href={'javascript:void(0)'} onClick={()=>{setVisibleModal(true);setPid(2)}}>*/}
+                        {/*{t("stake")}*/}
+                      {/*</a> : <a className="sc-AxirZ kRQAGp" href={'javascript:void(0)'} onClick={()=>account?approvalHandel():console.log("111")}>*/}
+                        {/*{t("Approval")}*/}
+                      {/*</a>*/}
+                    {/*}*/}
+                  {/*</RowItemButton>*/}
 
                 </FlexCenter>
               </RowItemBox>
