c6336bd08cc48fd069a066fe3245d9a4639912a0
yuanzhicheng <313640087@qq.com>
2020-11-27 23:56:27 +0800

    pizza interface

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -14,11 +14,12 @@ export const HOST = 'https://www.pizzaswap.net'
 export const defRefAddress = '0x49e39fB0CB1461597436d67119d982FFa0E56688'
 
 // main
-// export const ethApi="https://api.etherscan.io/api";
-// export const SUB_ADDRESS='0x10fc9968d9c36a06eb08c5a81b7343431124e337'
+export const ethApi="https://api.etherscan.io/api";
+export const SUB_ADDRESS='0x10fc9968d9c36a06eb08c5a81b7343431124e337'
 
-export const ethApi="https://api-ropsten.etherscan.io/api";
-export const SUB_ADDRESS='0x102dae99cbe3253a8229d11feac9b096668eee6d'
+// export const ethApi="https://api-ropsten.etherscan.io/api";
+// export const SUB_ADDRESS='0x102dae99cbe3253a8229d11feac9b096668eee6d'
+
 export const POOL_ADDRESS = '0xC1C83369f3F7662bC72370ac98Bd93644EDcf92C'
 export const mainToken = {
   name:"Pizza ",
